31543f75bb0f59d366e456fc94f6e4277557dec9
Suren Martirosyan <suren.v.mar@gmail.com>
2025-07-29 19:26:37 -0400

    Filtered data, kept only necessary rows and columns

--- ml/ml_models/train_bike_clf.py
+++ ml/ml_models/train_bike_clf.py
@@ -1,9 +1,10 @@
 """train_bike_clf trains the bike suitability classifier"""
 
-# Import ML libraries
+# Import libraries for ML
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import SGDClassifier
+import matplotlib.pyplot as plt
 
 # Import path libraries
 import joblib
@@ -14,6 +15,20 @@ CURRENT_DIR = os.path.dirname(__file__)
 bike_csv_path = os.path.join(CURRENT_DIR, '..', 'datasets', 'bike.csv')
 bike_csv_path = os.path.abspath(bike_csv_path)
 bike_df = pd.read_csv(bike_csv_path)
+
+
+bike_df.dropna(axis=0)
+
+# Note: We keep casual users because registered users are those who bike
+#   for communiting (to work, school, etc), not for leasure.
+bike_df.drop(['instant', 'dteday', 'season', 'yr', 'mnth', 'holiday',
+        'weekday', 'workingday', 'weathersit', 'atemp', 'registered', 'cnt'], 
+        axis=1)
+
+# Average bikers between 2 and 5pm are similar, so no third party variables
+#   such as time of day will affect our model
+bike_df = bike_df[(bike_df['hr'] >= 14) & (bike_df['hr'] <= 17)]
+
 
 
 
