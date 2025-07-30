66cc9aab2fd10ae93d379d1c92acef62e1164fa1
Suren Martirosyan <suren.v.mar@gmail.com>
2025-07-30 19:05:03 -0400

    Trained and tuned a random forest model with accuracy 88.4%

--- ml/ml_models/train_bike_clf.py
+++ ml/ml_models/train_bike_clf.py
@@ -1,53 +1,70 @@
 """train_bike_clf trains the bike suitability classifier"""
 
-# Import libraries for ML
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-
-# Import path libraries
 import joblib
 import os
 
+# Data analysis libraries
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+from sklearn.metrics import precision_recall_curve
+
+# ML libraries
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+
 # Get datasets and convert to df
 CURRENT_DIR = os.path.dirname(__file__)
-bike_csv_path = os.path.join(CURRENT_DIR, '..', 'datasets', 'bike.csv')
+bike_csv_path = os.path.join(CURRENT_DIR, '..', 'datasets', 'bike_weather.csv')
 bike_csv_path = os.path.abspath(bike_csv_path)
-bike_df = pd.read_csv(bike_csv_path)
+bike_df = pd.read_csv(bike_csv_path).dropna(axis=0)
 
-
-bike_ddf = bike_df.dropna(axis=0)
-
-# Average bikers between 2 and 5pm are similar, so no third party variables
+# Average biker count between 12 and 2pm are similar, so no third party variables
 #   such as time of day will affect our model
-bike_df = bike_df[(bike_df['hr'] >= 14) & (bike_df['hr'] <= 17)]
+bike_df = bike_df[(bike_df['Hour'] >= 12) & (bike_df['Hour'] <= 14)]
 
-# Note: We keep casual users because registered users are those who bike
-#   for communiting (to work, school, etc), not for leasure.
-bike_df = bike_df.drop(columns=['instant', 'dteday', 'season', 'yr', 'mnth', 
-        'holiday', 'weekday', 'workingday', 'weathersit', 'atemp', 'registered', 
-        'cnt', 'hr'])
+# Combine Rainfall and Snowfall under single Precipitation column
+MM_TO_CM = 10
+bike_df = bike_df.rename(columns={'Rainfall': 'Precipitation'})
+bike_df['Precipitation'] = np.where(
+    bike_df['Precipitation'] > bike_df['Snowfall'] * MM_TO_CM,
+    bike_df['Precipitation'], bike_df['Snowfall']) * MM_TO_CM
 
-# Create label column
-cutoff = int(bike_df['casual'].mean())
-bike_df['casual'] = np.where(bike_df['casual'] >= cutoff, 1, 0)
-bike_df = bike_df.rename(columns={'casual': 'biking_suitability'})
+# Drop unnecessary columns
+bike_df = bike_df.drop(columns=['Date', 'Hour', 'Visibility', 'Dew point temperature', 'Solar Radiation', 
+        'Holiday', 'Seasons', 'Functioning Day', 'Snowfall'])
 
-# Preprocess features and labels
-X = bike_df.drop(columns=(['biking_suitability']))
-y = bike_df['biking_suitability']
+# Create the label column
+cutoff = int(bike_df['Rented Bike Count'].median())
+bike_df['Rented Bike Count'] = np.where(bike_df['Rented Bike Count'] >= cutoff, 1, 0)
+bike_df = bike_df.rename(columns={'Rented Bike Count': 'biking_suitability'})
 
+# Create features and labels
+X = bike_df.drop(columns=(['biking_suitability'])).values
+y = bike_df['biking_suitability'].values
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, stratify=y, random_state=4,
+    X, y, test_size=0.2, stratify=y, random_state=4,
 )
-stdsc = StandardScaler()
-X_train_std = stdsc.fit_transform(X_train)
-X_test_std = stdsc.fit_transform(X_test)
 
+# Fine tune hyperparameters
+param_grid = {
+    'n_estimators': [100, 200],
+    'max_depth': [10, 12, 14],
+    'min_samples_split': [2, 5, 7],
+    'min_samples_leaf': [1, 2],
+    'max_features': ['sqrt'],
+    'bootstrap': [False]
+}
+grid = GridSearchCV(
+    estimator=RandomForestClassifier(random_state=4),
+    param_grid=param_grid,
+    scoring='accuracy',
+    cv=5,
+    n_jobs=3,
+    error_score='raise',
+)
 
-
-
-
+grid.fit(X_train, y_train)
+forest = grid.best_estimator_
