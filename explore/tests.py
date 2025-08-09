581f2b6307fc7cbb680a1de9f4050b8f91fc69c4
Suren Martirosyan <suren.v.mar@gmail.com>
2025-08-09 18:24:56 -0400

    Added test for world_map coord retrieval

--- explore/tests.py
+++ explore/tests.py
@@ -2,7 +2,8 @@
 
 import pytest
 from django.urls import reverse
-from explore import views
+from library.models import Destination
+
 
 @pytest.fixture
 def logged_in_client(client, django_user_model):
@@ -11,10 +12,18 @@ def logged_in_client(client, django_user_model):
     client.login(username="test", password="pass")
     return client, user
 
-@pytest.mark.django_db
+
+def test_world_map_redirect_if_not_logged_in(client):
+    response = client.get(reverse('explore:world_map'))
+
+    # Check for correct redirection
+    assert response.status_code == 302  # Default redirect status code
+    assert response.url.startswith('/accounts/login/')
+
+
 def test_render_world_map(logged_in_client):
     client, user = logged_in_client
-    response = client.get('/explore/')
+    response = client.get(reverse('explore:world_map'))
 
     # Check HTTP status
     assert response.status_code == 200
@@ -23,9 +32,24 @@ def test_render_world_map(logged_in_client):
     templates = [t.name for t in response.templates if t.name is not None]
     assert 'explore/world_map.html' in templates
 
-def test_world_map_redirect_if_not_logged_in(client):
-    url = reverse('explore:world_map')
-    response = client.get(url)
+    # Check for forms
+    assert 'country_form' in response.context
+    assert 'city_form' in response.context
 
-    assert response.status_code == 302  # Default redirect status code
-    assert response.url.startswith('/accounts/login/')
+
+@pytest.mark.django_db
+def test_coord_retrieval_from_Destination_model(logged_in_client):
+    client, user = logged_in_client
+    
+    # Create entry in Destination model
+    Destination.objects.create(
+        owner=user,
+        latitude=10.0,
+        longitude=20.0,
+    )
+    response = client.get(reverse('explore:world_map'))
+
+    # Check for correct coord retrieval
+    assert len(response.context["destinations"]) == 1
+    assert response.context["destinations"][0]["latitude"] == 10.0
+    assert response.context["destinations"][0]["longitude"] == 20.0
